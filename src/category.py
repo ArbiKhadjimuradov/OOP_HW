1176c8ab8636149d7f7b8cdec914eaec17eba1d1
Mihouk <arbi_14@mail.ru>
2024-08-06 13:32:26 +0300

    start

--- src/category.py
+++ src/category.py
@@ -1,7 +1,15 @@
-
-
-
-
-
 class Category:
-    pass
+    name = str
+    description = str
+    produts = list
+    count_category = 0
+    count_products = 0
+
+    def __init__(self, name, description, produts=None):
+        self.product = None
+        self.name = name
+        self.description = description
+        self.produts = produts if produts else []
+
+        Category.count_category += 1
+        Category.count_products += len(produts) if produts else 0
--- src/product.py
+++ src/product.py
@@ -1,12 +1,11 @@
-
-
-
-
 class Product:
     name: str #название
     description: str #описание
-    price: float #цена
+    price: (float, 1) #цена
     quantity: int #количество
 
-    def __init__(self):
-        pass
+    def __init__(self, name, description, price, quantity):
+        self.name = name
+        self.description = description
+        self.quantity = quantity
+        self.price = price
