9fb48191b20b0e10c92a92aed4a482d4d557c30d
Mihouk <arbi_14@mail.ru>
2024-08-15 22:07:38 +0300

    добавил приватный аттребут в category.py и декоратор

--- src/category.py
+++ src/category.py
@@ -1,15 +1,25 @@
 class Category:
-    name = str #название категории
-    description = str #описание
-    products = list #лист с продуктами
-    count_category = 0 #количество категорий
-    count_products = 0 #количество продуктов
-
+    name = str  #название категории
+    description = str  #описание
+    products = list  #лист с продуктами
+    count_category = 0  #количество категорий
+    count_products = 0  #количество продуктов
 
     def __init__(self, name, description, products=None):
         self.name = name
         self.description = description
-        self.products = products if products else []
+        self.__products = products if products else []
 
         Category.count_category += 1
         Category.count_products += len(products) if products else 0
+
+    def add_product(self, products):
+        self.__products.append(products)
+        Category.count_products += 1
+
+    @property
+    def products(self):
+        products_str = ""
+        for product in self.__products:
+            products_str += f"{product.name}, {product.price} руб. Остаток: {product.quantity} шт.\n"
+        return products_str
